aa9fa6c435ce7338ce589f21c4f5d02e5a658ef8
AsepSkizo <septarahmat27@gmail.com>
2024-04-26 09:53:02 +0700

    Membuat relasi antar tabel

--- app/Models/harian.php
+++ app/Models/harian.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class harian extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * Mendapatkan data rekening terkait
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function rekening(): BelongsTo
+    {
+        return $this->belongsTo(rekening::class, 'kode_rekening', 'kode_rekening');
+    }
 }
--- app/Models/rekening.php
+++ app/Models/rekening.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class rekening extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    /**
+     * Mendapatkan data target yang terkait dengan rekening
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function target(): HasMany
+    {
+        return $this->hasMany(target::class, 'kode_rekening', 'kode_rekening');
+    }
+
+    /**
+     * Mendapatkan data harian yang terkait dengan rekening
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function harian(): HasMany
+    {
+        return $this->hasMany(harian::class, 'kode_rekening', 'kode_rekening');
+    }
 }
